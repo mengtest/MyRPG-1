3c2ce68e643deeb3c9767c6a8668aaa2a3d3db4a
zuig <zuig@163.com>
2015-08-31 17:26:22 +0800

    20150831

---
+++ Assets/Scripts/Controller/BattleController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BattleController : StateMachine 
+{
+	public GameObject heroPrefab;
+	public CameraRig cameraRig;
+	public Board board;
+	public LevelData levelData;
+	public Transform tileSelectionIndicator;
+	public Point pos;
+	public Tile currentTile { get { return board.GetTile(pos); }}
+	public AbilityMenuPanelController abilityMenuPanelController;
+	public StatPanelController statPanelController;
+	public Turn turn = new Turn();
+	public List<Unit> units = new List<Unit>();
+
+	void Start ()
+	{
+		ChangeState<InitBattleState>();
+	}
+}
